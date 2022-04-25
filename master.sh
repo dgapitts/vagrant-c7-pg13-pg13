508f3e6878d52c078226be8dce2b3544254477ed
Dave Pitts <dgapitts@gmail.com>
2022-04-26 00:47:06 +0200

    new shorter and cleaner provision_base.sh script

--- master.sh
+++ master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /vagrant/provision.sh
+source /vagrant/provision_base.sh
 
 echo "The master-only steps"
 
@@ -8,6 +8,19 @@ echo "The master-only steps"
 systemctl enable postgresql-13.service
 systemctl start postgresql-13.service 
 
+
+# setup bench1 linux user
+adduser bench1
+# setup bench1 postgres user and database
+su -c "createuser bench1" -s /bin/sh postgres
+su -c "createdb bench1"  -s /bin/sh postgres
+# setup vagrant postgres user and database
+su -c "createuser vagrant" -s /bin/sh postgres
+su -c "createdb vagrant"  -s /bin/sh postgres
+
+# initial data load for 
+su -c "/usr/pgsql-13/bin/pgbench -i -s 30" -s /bin/sh bench1
+su -c "/usr/pgsql-13/bin/pgbench -i -s 3" -s /bin/sh vagrant
 
 
 sudo -u postgres -H sh -c "psql -c 'CREATE USER rep REPLICATION LOGIN CONNECTION LIMIT 2;'"
---
+++ provision_base.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+if [ ! -f /home/vagrant/already-installed-flag ]
+then
+  echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
+  echo "ADD EXTRA ALIAS VIA .bashrc"
+  echo 'export PATH="$PATH:/usr/pgsql-13/bin"' >> /vagrant/bashrc.append.txt
+  cat /vagrant/bashrc.append.txt >> /home/vagrant/.bashrc
+  cat /vagrant/bashrc.append.txt >> /root/.bashrc 
+  echo "alias pg='sudo su - postgres'" >> /home/vagrant/.bashrc
+  echo "alias bench='sudo su - bench1'" >> /home/vagrant/.bashrc
+
+
+  #echo "GENERAL YUM UPDATE"
+  #yum -y update
+  #echo "INSTALL GIT"
+
+
+  # Regular pg13 install                                                                           
+  # As of 15 April 2019, there is only one repository RPM per distro, and it includes repository information for all available PostgreSQL releases.
+  # rpm -ivh https://yum.postgresql.org/9.6/redhat/rhel-7.3-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+  # rpm -Uvh https://yum.postgresql.org/13/redhat/rhel-7-x86_64/pgdg-centos13-13-2.noarch.rpm
+  # https://yum.postgresql.org/repopackages/
+  rpm -ivh https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+
+  yum -y update
+  yum -y install postgresql13-server postgresql13
+  yum -y install postgresql13-contrib postgresql13-libs postgresql13-devel
+ 
+  # Extra packages and devtool for install of https://github.com/ossc-db/pg_plan_advsr  
+  #yum -y install postgresql-devel
+  #yum -y install gcc
+  
+  # prevent vagrant user ssh warning "setlocale: LC_CTYPE: cannot change locale (UTF-8): No such file or directory"
+  sudo cat /vagrant/environment >> /etc/environment
+
+  # nice setup shortcut as per https://github.com/philmcc/postgres_clusters
+  ln -s /var/lib/pgsql/13/data /database
+  cd /database
+
+
+
+else
+  echo "already installed flag set : /home/vagrant/already-installed-flag"
+fi
+
